316a0338ee6bf47396855dfc8163f6030611cdd1
Cristian Martinez <cristianandres92@gmail.com>
2021-06-12 15:34:55 -0500

    - new auth service added - new spinner component added - new user authentication model added

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {AuthResponseData, AuthService} from './auth.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -7,13 +9,39 @@ import {NgForm} from '@angular/forms';
 })
 export class AuthComponent {
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
+
+  constructor(private authService: AuthService) {
+  }
 
   public onSwitchMode(): void {
     this.isLoginMode = !this.isLoginMode;
   }
 
   public onSubmit(authForm: NgForm): void {
-    console.log(authForm.value);
+    if (authForm.invalid) {
+      return;
+    }
+
+    const email: string = authForm.value.email;
+    const password: string = authForm.value.password;
+
+    this.isLoading = true;
+
+    const authObservable: Observable<AuthResponseData> = this.isLoginMode ?
+      this.authService.login(email, password) :
+      this.authService.signUp(email, password);
+
+    authObservable.subscribe(responseData => {
+      console.log(responseData);
+      this.isLoading = false;
+    }, errorMessage => {
+      this.error = errorMessage;
+      console.error(errorMessage);
+      this.isLoading = false;
+    });
+
     authForm.reset();
   }
 }
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,78 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable, Subject, throwError} from 'rxjs';
+import {catchError, tap} from 'rxjs/operators';
+import {UserModel} from './user.model';
+
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  userSubject = new Subject<UserModel>();
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  public signUp(email: string, password: string): Observable<AuthResponseData> {
+    return this.httpClient.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD_fw8EUo0AS8PAXEQiRYTxUwpCNDeAMfc', {
+      email,
+      password,
+      returnSecureToken: true
+    }).pipe(
+      catchError(this.handleError),
+      tap(this.handlerUserAuthentication)
+    );
+  }
+
+  public login(email: string, password: string): Observable<AuthResponseData> {
+    return this.httpClient.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD_fw8EUo0AS8PAXEQiRYTxUwpCNDeAMfc', {
+      email,
+      password,
+      returnSecureToken: true
+    }).pipe(catchError(this.handleError));
+  }
+
+  private handlerUserAuthentication = (responseData: AuthResponseData) => {
+    const timestamp: number = new Date().getTime() + +responseData.expiresIn * 1000;
+    const expirationDate = new Date(timestamp);
+    const user = new UserModel(responseData.localId,
+      responseData.email, responseData.idToken, expirationDate);
+    this.userSubject.next(user);
+  }
+
+  private handleError(errorResponse: HttpErrorResponse): Observable<never> {
+    let errorMessage = 'An unknown error occurs';
+
+    if (!errorResponse.error || !errorResponse.error.error) {
+      return throwError(errorMessage);
+    }
+
+    switch (errorResponse.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email already exist!';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'The password is invalid or the user does not have a password.';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted.';
+        break;
+      case 'USER_DISABLED':
+        errorMessage = 'The user account has been disabled by an administrator.';
+        break;
+    }
+
+    return throwError(errorMessage);
+  }
+}
