b5672a21dbce46b72576da1ac0a911a9bab8a407
Cristian Martinez <cristianandres92@gmail.com>
2021-06-13 13:22:57 -0500

    adding a new alert component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, ComponentFactoryResolver} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {AuthResponseData, AuthService} from './auth.service';
 import {Observable} from 'rxjs';
 import {Router} from '@angular/router';
+import {AlertComponent} from '../shared/alert/alert.component';
 
 @Component({
   selector: 'app-auth',
@@ -13,7 +14,8 @@ export class AuthComponent {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService, private router: Router,
+              private componentFactoryResolver: ComponentFactoryResolver) {
   }
 
   public onSwitchMode(): void {
@@ -42,6 +44,7 @@ export class AuthComponent {
 
     const handleErrorLogin = (errorMessage) => {
       this.error = errorMessage;
+      this.showErrorAlert(errorMessage);
       console.error(errorMessage);
       this.isLoading = false;
     };
@@ -49,5 +52,15 @@ export class AuthComponent {
     authObservable.subscribe(handleSuccessLogin, handleErrorLogin);
 
     authForm.reset();
+  }
+
+  public onHandleError(): void {
+    this.error = null;
+  }
+
+  private showErrorAlert(message: string): void {
+    const alertComponentComponentFactory = this.componentFactoryResolver
+      .resolveComponentFactory(AlertComponent);
+
   }
 }
